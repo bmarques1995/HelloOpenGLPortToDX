77856475fc4fd683897bbfc2bb254d44290a7b57
B Marques <brunosmarques1995@hotmail.com>
2023-01-21 09:52:33 -0300

    moved viewports call to draw

--- src/lib/graphics/d3d11/D3D11Context.cpp
+++ src/lib/graphics/d3d11/D3D11Context.cpp
@@ -55,6 +55,7 @@ void APILearning::D3D11Context::Present()
 
 void APILearning::D3D11Context::Draw(uint32_t elements)
 {
+    m_DeviceContext->RSSetViewports(1, &m_Viewport);
     m_DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
     m_DeviceContext->DrawIndexedInstanced(elements, 1, 0, 0, 0);
 }
@@ -141,6 +142,4 @@ void APILearning::D3D11Context::SetViewport(uint32_t width, uint32_t height)
     m_Viewport.Height = (float)height;
     m_Viewport.MinDepth = .0f;
     m_Viewport.MaxDepth = 1.0f;
-
-    m_DeviceContext->RSSetViewports(1, &m_Viewport);
 }
